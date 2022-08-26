49539dffe917404eeae059cd7640be5b6c6ee4dd
itsMrApon <Shifat15-11191@diu.edu.bd>
2022-08-26 20:02:10 +0600

    final assignmet

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+// link
+function relocate_home()
+{
+     location.href = "www.instagram.com/itsmrapon";
+} 
+// link
+function relocate_play()
+{
+     location.href = "https://play.google.com/store/apps/details?id=com.adobe.creativeapps.gather&hl=en&gl=US";
+} 
+// link
+function relocate_store()
+{
+     location.href = "https://apps.apple.com/us/app/adobe-illustrator-graphic-art/id1018784575";
+} 
